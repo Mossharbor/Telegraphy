bb1a38efc17adef6529356d235125d02e16dafeb
Matthew Johnson <mattj@mossharbor.com>
2018-05-04 09:48:32 -0700

    Added serice bus queue dead letter wrapper

---
+++ TelegraphAzure/AzureWrappers/ServiceBusDeadLetterQueue.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegraphy.Azure
+{
+    class ServiceBusDeadLetterQueue : ServiceBusQueue
+    {
+        public ServiceBusDeadLetterQueue(string connectionString, string entityPath, ReceiveMode receiveMode = ReceiveMode.PeekLock, RetryPolicy retryPolicy = null)
+            : base(connectionString, EntityNameHelper.FormatDeadLetterPath(entityPath), false, receiveMode, retryPolicy)
+        {
+        }
+    }
+}
