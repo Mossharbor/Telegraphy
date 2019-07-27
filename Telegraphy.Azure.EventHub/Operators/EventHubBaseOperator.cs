2123bb75269d7f00cf04e67d6b200ca781f89a1b
Matthew Johnson <mattj@mossharbor.com>
2019-07-27 05:21:54 -0700

    Added error handling and ask support to event hub

--- Telegraphy.Azure.EventHub/Operators/EventHubBaseOperator.cs
+++ Telegraphy.Azure.EventHub/Operators/EventHubBaseOperator.cs
@@ -63,47 +63,73 @@ namespace Telegraphy.Azure
         
         public void AddMessage(IActorMessage msg)
         {
-            EventData eventData = SendBytesToEventHub.BuildMessage<MsgType>(msg);
-            EventHubClient.Send(eventData);
+            try
+            {
+                EventData eventData = SendBytesToEventHub.BuildMessage<MsgType>(msg);
+                EventHubClient.Send(eventData);
             
-            if (null == msg.Status)
-                msg.Status = new TaskCompletionSource<IActorMessage>();
+                if (null == msg.Status)
+                    msg.Status = new TaskCompletionSource<IActorMessage>();
 
-            msg.Status?.SetResult(new EventHubMessage(eventData));
+                msg.Status?.SetResult(new EventHubMessage(eventData));
+            }
+            catch (Exception ex)
+            {
+                Exception foundEx = null;
+                var handler = this.FindExceptionHandler(_exceptionTypeToHandler, ex, out foundEx);
+
+                if (null != handler)
+                    handler.Invoke(foundEx);
+
+                if (null != msg.Status && !msg.Status.Task.IsCanceled)
+                    msg.Status.TrySetException(ex);
+            }
         }
 
         public IActorMessage GetMessage()
         {
-            if (1 == maxDequeueCount)
+            try
             {
-                var recievedSingle = this.EventHubMsgReciever.Recieve(maxDequeueCount, null);
-                if (null == recievedSingle || 0 == recievedSingle.Count())
+                if (1 == maxDequeueCount)
+                {
+                    var recievedSingle = this.EventHubMsgReciever.Recieve(maxDequeueCount, null);
+                    if (null == recievedSingle || 0 == recievedSingle.Count())
+                        return null;
+
+                    byte[] msgBytes = null;
+                    msgBytes = recievedSingle.First().Body.Array;
+                    return ConvertToActorMessage(msgBytes);
+                }
+
+                while (0 != msgQueue.Count)
+                {
+                    IActorMessage msg = null;
+                    if (msgQueue.TryDequeue(out msg))
+                        return msg;
+                }
+
+                var recievedList = this.EventHubMsgReciever.Recieve(maxDequeueCount, null);
+                if (null == recievedList || 0 == recievedList.Count())
                     return null;
 
-                byte[] msgBytes = null;
-                msgBytes = recievedSingle.First().Body.Array;
-                return ConvertToActorMessage(msgBytes);
-            }
+                for (int i = 1; i < recievedList.Count(); ++i)
+                {
+                    byte[] msgBytes = recievedList.ElementAt(i).Body.Array;
+                    msgQueue.Enqueue(ConvertToActorMessage(msgBytes));
+                }
 
-            while (0 != msgQueue.Count)
-            {
-                IActorMessage msg = null;
-                if (msgQueue.TryDequeue(out msg))
-                    return msg;
+                // save the first one to return
+                return ConvertToActorMessage(recievedList.ElementAt(0).Body.Array);
             }
-           
-            var recievedList = this.EventHubMsgReciever.Recieve(maxDequeueCount, null);
-            if (null == recievedList || 0 == recievedList.Count())
+            catch (Exception ex)
+            {
+                Exception foundEx = null;
+                var handler = this.FindExceptionHandler(_exceptionTypeToHandler, ex, out foundEx);
+
+                if (null != handler)
+                    handler.Invoke(foundEx);
                 return null;
-
-            for(int i=1; i < recievedList.Count(); ++i)
-            {
-                byte[] msgBytes = recievedList.ElementAt(i).Body.Array;
-                msgQueue.Enqueue(ConvertToActorMessage(msgBytes));
             }
-
-            // save the first one to return
-            return ConvertToActorMessage(recievedList.ElementAt(0).Body.Array);
         }
         
         public void Kill()
