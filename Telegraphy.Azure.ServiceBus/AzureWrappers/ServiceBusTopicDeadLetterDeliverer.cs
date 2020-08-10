dc10600103ef16517926554484800afc38dc7a1e
Matt Johnson <mattj@microsoft.com>
2020-08-10 14:10:28 -0700

    Updated linked version for relay and serice bus

--- Telegraphy.Azure.ServiceBus/AzureWrappers/ServiceBusTopicDeadLetterDeliverer.cs
+++ Telegraphy.Azure.ServiceBus/AzureWrappers/ServiceBusTopicDeadLetterDeliverer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.ServiceBus;
+﻿
+using Microsoft.Azure.ServiceBus;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Telegraphy.Azure.ServiceBus/Messages/ServiceBusMessage.cs
+++ Telegraphy.Azure.ServiceBus/Messages/ServiceBusMessage.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.Azure.ServiceBus;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telegraphy.Net;
+using Microsoft.Azure.ServiceBus;
 
 namespace Telegraphy.Azure
 {
-    internal class ServiceBusMessage : SimpleMessage<Microsoft.Azure.ServiceBus.Message>
+    internal class ServiceBusMessage : SimpleMessage<Message>
     {
         public ServiceBusMessage(Microsoft.Azure.ServiceBus.Message message) : base(message)
         {
